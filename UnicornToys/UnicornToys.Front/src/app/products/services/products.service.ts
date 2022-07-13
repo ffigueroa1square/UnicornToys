83fdee7fb13e2a55055c307c99dd17e732d7b0b5
Francisco Figueroa <francisco.figueroa@unosquare.com>
2022-07-13 10:53:09 -0700

    add unit test to products components and services

--- UnicornToys/UnicornToys.Front/src/app/products/services/products.service.ts
+++ UnicornToys/UnicornToys.Front/src/app/products/services/products.service.ts
@@ -1,33 +1,33 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { environment } from "src/environments/environment";
-import { Product } from "../models/product.model";
-import { ProductDto } from "../models/productDto.model";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Product } from '../models/product.model';
+import { ProductDto } from '../models/productDto.model';
 
 @Injectable()
 export class ProductsService {
-    private url = `${environment.apiUrl}/products`;
-    
-    constructor(private _http: HttpClient) { }
+  private url = `${environment.apiUrl}/products`;
 
-    public getProducts() : Observable<Product[]> {
-        return this._http.get<Product[]>(`${this.url}`);
-    }
+  constructor(private _http: HttpClient) {}
 
-    public get(id: string): Observable<Product> {
-        return this._http.get<Product>(`${this.url}/${id}`);
-    }
+  public getProducts(): Observable<Product[]> {
+    return this._http.get<Product[]>(`${this.url}`);
+  }
 
-    public post(entity: ProductDto): Observable<any> {
-        return this._http.post<any>(`${this.url}`, entity);
-    }
+  public get(id: string): Observable<Product> {
+    return this._http.get<Product>(`${this.url}/${id}`);
+  }
 
-    public put(id: string, entity: Product): Observable<any> {
-        return this._http.put<any>(`${this.url}/${id}`, entity);
-    }
+  public post(entity: ProductDto): Observable<boolean> {
+    return this._http.post<boolean>(`${this.url}`, entity);
+  }
 
-    public delete(id: string): Observable<any> {
-        return this._http.delete<any>(`${this.url}/${id}`);
-      }
+  public put(id: string, entity: Product): Observable<boolean> {
+    return this._http.put<boolean>(`${this.url}/${id}`, entity);
+  }
+
+  public delete(id: string): Observable<boolean> {
+    return this._http.delete<boolean>(`${this.url}/${id}`);
+  }
 }
