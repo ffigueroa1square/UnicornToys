caa18ab48f446a8e263c9897cb62e9646b77fac3
Francisco Figueroa <francisco.figueroa@unosquare.com>
2022-07-18 18:17:16 -0700

    set up serilog library

--- UnicornToys/UnicornToys.API/Program.cs
+++ UnicornToys/UnicornToys.API/Program.cs
@@ -4,6 +4,7 @@ using MediatR;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
+using Serilog;
 using System.Reflection;
 using UnicornToys.API.Middlewares;
 using UnicornToys.Application.Features.Behaviors;
@@ -12,91 +13,116 @@ using UnicornToys.Application.Mappings;
 using UnicornToys.Persistence;
 using UnicornToys.Persistence.Contexts;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-// Add services to the container.
+Log.Information("Starting up");
 
-builder.Services.AddDbContext<MyAppDbContext>(options =>
+try
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("AppConnection"));
-});
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", p =>
+    // Add services to the container.
+
+    builder.Services.AddDbContext<MyAppDbContext>(options =>
     {
-        p.AllowAnyOrigin()
-        .AllowAnyHeader()
-        .AllowAnyMethod();
+        options.UseSqlServer(builder.Configuration.GetConnectionString("AppConnection"));
     });
-});
 
-builder.Services.Configure<FormOptions>(o =>
-{
-    o.ValueLengthLimit = int.MaxValue;
-    o.MultipartBodyLengthLimit = int.MaxValue;
-    o.MemoryBufferThreshold = int.MaxValue;
-});
-
-//Register AutoMapper
-builder.Services.AddAutoMapper(typeof(Program).Assembly, typeof(AutoMapperProfile).Assembly);
-
-// Call UseServiceProviderFactory on the Host sub property
-builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
-
-builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
-{
-    containerBuilder.RegisterGeneric(typeof(Repository<>))
-        .As(typeof(IRepository<>))
-        .InstancePerRequest();
-
-    containerBuilder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerLifetimeScope();
-
-    containerBuilder.RegisterType<ExceptionHandlingMiddleware>().InstancePerDependency();
-
-    containerBuilder.RegisterAssemblyTypes(typeof(IMediator).GetTypeInfo().Assembly)
-        .AsImplementedInterfaces();
-
-    containerBuilder.RegisterModule<ProductModule>();    
-    containerBuilder.RegisterGeneric(typeof(ValidationBehavior<,>)).As(typeof(IPipelineBehavior<,>));
-
-    containerBuilder.Register<ServiceFactory>(ctx =>
+    builder.Services.AddCors(options =>
     {
-        var c = ctx.Resolve<IComponentContext>();
-        return t => c.Resolve(t);
-    });               
-});
+        options.AddPolicy("AllowAll", p =>
+        {
+            p.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+    });
 
-builder.Services.AddControllers();
+    builder.Services.Configure<FormOptions>(o =>
+    {
+        o.ValueLengthLimit = int.MaxValue;
+        o.MultipartBodyLengthLimit = int.MaxValue;
+        o.MemoryBufferThreshold = int.MaxValue;
+    });
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    //Register AutoMapper
+    builder.Services.AddAutoMapper(typeof(Program).Assembly, typeof(AutoMapperProfile).Assembly);
 
-var app = builder.Build();
+    // Call UseServiceProviderFactory on the Host sub property
+    builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
+    {
+        containerBuilder.RegisterGeneric(typeof(Repository<>))
+            .As(typeof(IRepository<>))
+            .InstancePerRequest();
+
+        containerBuilder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerLifetimeScope();
+
+        containerBuilder.RegisterType<ExceptionHandlingMiddleware>().InstancePerDependency();
+
+        containerBuilder.RegisterAssemblyTypes(typeof(IMediator).GetTypeInfo().Assembly)
+            .AsImplementedInterfaces();
+
+        containerBuilder.RegisterModule<ProductModule>();
+        containerBuilder.RegisterGeneric(typeof(ValidationBehavior<,>)).As(typeof(IPipelineBehavior<,>));
+        containerBuilder.RegisterGeneric(typeof(LoggingBehaviour<,>)).As(typeof(IPipelineBehavior<,>));
+
+        containerBuilder.Register<ServiceFactory>(ctx =>
+        {
+            var c = ctx.Resolve<IComponentContext>();
+            return t => c.Resolve(t);
+        });
+    });
+
+    builder.Host.UseSerilog((ctx, lc) => lc
+    .WriteTo.Console()
+    .ReadFrom.Configuration(ctx.Configuration));
+
+    builder.Services.AddControllers();
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseCors("AllowAll");
+
+    app.UseStaticFiles();
+    app.UseStaticFiles(new StaticFileOptions()
+    {
+        FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"Resources")),
+        RequestPath = new PathString("/Resources")
+    });
+
+    app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseCors("AllowAll");
-
-app.UseStaticFiles();
-app.UseStaticFiles(new StaticFileOptions()
+catch (Exception ex)
 {
-    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"Resources")),
-    RequestPath = new PathString("/Resources")
-});
-
-app.UseMiddleware<ExceptionHandlingMiddleware>();
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
